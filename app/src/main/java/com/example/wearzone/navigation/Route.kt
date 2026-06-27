a3bc54a10debd3150403222a5c7577ae1a623ee6
Aalaa-Adel <aalaaadel02@gmail.com>
2026-06-27 14:04:25 +0300

    add base navigation graph

---
+++ app/src/main/java/com/example/wearzone/navigation/Route.kt
@@ -0,0 +1,24 @@
+package com.example.wearzone.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Route{
+
+    @Serializable
+    data object SplashRoute : Route
+
+    // Aalaa
+
+
+    // Hend
+
+
+    // Ahmed
+
+
+    // Omer
+
+
+
+}
