fb1585c5ce67f801a96c5b892bbb39e2db0a8bc6
Hendsayed25 <hendsayed853@gmail.com>
2026-06-29 10:20:46 +0300

    Simplify import statement for RegisterScreen in NavGraph

--- app/src/main/java/com/example/wearzone/navigation/NavGraph.kt
+++ app/src/main/java/com/example/wearzone/navigation/NavGraph.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.presentation.auth.register.RegisterScreen
 
 @Composable
 fun NavGraph(
@@ -39,7 +40,7 @@ fun NavGraph(
 
         // Hend
         composable<Route.RegisterRoute> {
-            com.example.presentation.auth.register.RegisterScreen(
+           RegisterScreen(
                 onNavigateToHome = {
                     // TODO: Navigate to HomeRoute
                 },
