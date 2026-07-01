5b6da066b02b2c33eddffce71bad29ee072cde2f
Hendsayed25 <hendsayed853@gmail.com>
2026-07-01 23:08:19 +0300

    feat : add CartItemEntity and CartDao to WearZoneDatabase

--- data/src/main/kotlin/com/example/wearzone/data/db/WearZoneDatabase.kt
+++ data/src/main/kotlin/com/example/wearzone/data/db/WearZoneDatabase.kt
@@ -4,8 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.wearzone.data.local.search.RecentSearchDao
 import com.example.wearzone.data.local.search.RecentSearchEntity
+import com.example.wearzone.data.local.dao.CartDao
+import com.example.wearzone.data.local.entity.CartItemEntity
 
-@Database(entities = [WearZoneEntity::class, RecentSearchEntity::class], version = 2)
+@Database(entities = [WearZoneEntity::class, RecentSearchEntity::class,CartItemEntity::class], version = 3)
 abstract class WearZoneDatabase : RoomDatabase() {
     abstract fun recentSearchDao(): RecentSearchDao
+    abstract fun cartDao(): CartDao
 }
