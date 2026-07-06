e0ef518ce3b5d7cc473477355e2835ea04bb4e8b
Aalaa-Adel <aalaaadel02@gmail.com>
2026-07-06 16:46:19 +0300

    Refactor address map picker

--- data/src/main/kotlin/com/example/wearzone/data/local/search/RecentSearchEntity.kt
+++ data/src/main/kotlin/com/example/wearzone/data/local/search/RecentSearchEntity.kt
@@ -3,6 +3,7 @@ package com.example.wearzone.data.local.search
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.wearzone.domain.search.model.RecentSearch
 
 @Entity(tableName = "recent_searches")
 data class RecentSearchEntity(
@@ -10,4 +11,11 @@ data class RecentSearchEntity(
     val query: String,
     @ColumnInfo(name = "searched_at")
     val searchedAt: Long,
+) {
+    fun toDomain(): RecentSearch = RecentSearch(query = query, searchedAt = searchedAt)
+}
+
+fun RecentSearch.toDto(): RecentSearchEntity = RecentSearchEntity(
+    query = query,
+    searchedAt = searchedAt,
 )
